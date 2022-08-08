bf70cfae930a2297d91a635322b19cad771989f5
s-polat <spolatde@gmail.com>
2022-08-08 12:11:31 +0200

    material Ui kullanimi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Person } from "@mui/icons-material";
+import { Button, Typography } from "@mui/material";
+import { styled } from '@mui/material/styles';
+
+
 
 function App() {
+  const StyledButton = styled(Button)({
+    backgroundColor: "red",
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <Button></Button>
+      <Button variant="text" style={{backgroundColor:"white", color:"purple"}}>Text</Button> {/* //burda kendi style imizi inline olarak verdik */}
+      <Button variant="contained" sx={{backgroundColor:"purple", color:"white"}}>Text</Button> {/* //burdada kendi style imizi inline "sx" olarak verdik */}
+      <StyledButton variant="contained">Siddik</StyledButton>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined" >Outlined</Button>
+      <Button startIcon={<Person/>} variant="contained" color="success" >Outlined</Button>
+      <Button variant="contained" disableElevation>
+        Disable elevation
+      </Button>
+      <Typography variant="h3" component="p">Hello World</Typography>
+
     </div>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#00bcd4",
+            light: "skyblue",
+        },
+        secondary: {
+            main: "#ff4081",
+        },
+        otherColor: {
+            main: "#ff0000",
+        }
+    }
+    
+})
